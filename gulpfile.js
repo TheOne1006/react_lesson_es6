fee5ce2c00353a90e8aae893640e0e379f3d5d21
theone <297190869@qq.com>
2016-01-27 23:27:17 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+/**
+ * use in browser
+ */
+var browserSync = require('browser-sync'),
+    reload      = browserSync.reload;
+
+gulp.task('browser-sync', function() {
+
+    browserSync.init({
+        // 服务开启地址
+        server: './',
+        index: "index.html",
+        directory: true,
+        // Linux
+        // browser: ["chromium-browser"]
+        // Mac
+        // browser: ["Google Chrome", "firefox"]
+        browser: ["Google Chrome"]
+    });
+
+});
+
+gulp.task('watchFile', function(){
+
+    gulp.watch([
+			'lesson01/*.*',
+      ])
+      .on('change', reload);
+});
+
+gulp.task('serve', ['watchFile','browser-sync'], function(){
+});
